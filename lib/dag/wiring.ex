3e8c739e22493392bd0babf070eb690693794a57
Christophe De Troyer <christophe.detroyer@gmail.com>
2020-04-10 16:08:15 +0200

    dag merge

--- lib/dag/wiring.ex
+++ lib/dag/wiring.ex
@@ -16,6 +16,21 @@ defmodule Creek.Wiring do
     m_dag
   end
 
+  @doc """
+  Merge two dags together
+  """
+  def lefts ~>> right do
+    merge(lefts, right)
+  end
+
+  defp merge([leftx, lefty], right) do
+    l_dagx = ensure_dag(leftx)
+    l_dagy = ensure_dag(lefty)
+    r_dag = ensure_dag(right)
+
+    threeway_merge(l_dagx, l_dagy, r_dag)
+  end
+
   # -----------------------------------------------------------------------------
   # Helpers
 
@@ -50,5 +65,29 @@ defmodule Creek.Wiring do
       MutableGraph.add_edge(left, from, to)
     end)
     |> MutableGraph.add_edge(l_sink, r_source)
+  end
+
+  defp threeway_merge(leftx, lefty, right) do
+    # Determine the sink and source node for each graph, where they will be connected.
+    lx_sink = MutableGraph.sink_vertices(leftx) |> hd()
+    ly_sink = MutableGraph.sink_vertices(lefty) |> hd()
+    r_source = MutableGraph.source_vertices(right) |> hd()
+
+    # Merge the graphs together.
+    right =
+      leftx
+      |> MutableGraph.edges()
+      |> Enum.reduce(right, fn {from, to}, left ->
+        MutableGraph.add_edge(left, from, to)
+      end)
+      |> MutableGraph.add_edge(lx_sink, r_source)
+
+    # Merge the graphs together.
+    lefty
+    |> MutableGraph.edges()
+    |> Enum.reduce(right, fn {from, to}, left ->
+      MutableGraph.add_edge(left, from, to)
+    end)
+    |> MutableGraph.add_edge(ly_sink, r_source)
   end
 end
