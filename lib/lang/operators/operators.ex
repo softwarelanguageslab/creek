d9febc94bbb08277aa80ec44fba1129dec2e8055
Christophe De Troyer <christophe.detroyer@gmail.com>
2022-03-03 16:28:53 +0100

    fix bug in average

--- lib/lang/operators/operators.ex
+++ lib/lang/operators/operators.ex
@@ -168,7 +168,7 @@ defmodule Creek.Operator do
       if count == nil and sum == nil do
         {{1, v}, v}
       else
-        count = if count >= last, do: count + 1, else: 0
+        count = if count >= last, do: count + 1, else: 1
         sum = if count >= last, do: v, else: sum + v
         IO.inspect sum, label: "sum"
         IO.inspect count, label: "count"
